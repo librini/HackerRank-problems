7ed33f35487fc646a87031a9c406568ba0556952
Anas Librini <anlibrin@c2r9p3.1337.ma>
2025-09-28 15:56:34 +0100

    is done

---
+++ C_problems/SumofDigitsofaFiveDigitNumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	
+    int n;
+    int res = 0;
+    scanf("%d", &n);
+    while(n)
+    {
+        res += n % 10;
+        n /= 10;
+    }
+    printf("%d\n",res);
+    return 0;
+}
